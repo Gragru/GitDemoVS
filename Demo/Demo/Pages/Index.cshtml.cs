3d9102343ba86cd46ef8f44e79c5a96245824a57
Mikael Engström <micke@primat.se>
2021-10-20 11:50:02 +0200

    Update Index.cshtml.cs
    Lagt till variabel x

--- Demo/Demo/Pages/Index.cshtml.cs
+++ Demo/Demo/Pages/Index.cshtml.cs
@@ -10,16 +10,11 @@ namespace Demo.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
 
         public void OnGet()
         {
-
+            var x = 100;
         }
     }
 }
